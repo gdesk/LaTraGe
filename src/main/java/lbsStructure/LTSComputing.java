c36638c35dabd8c36c59d156dd695acfdf41d8d5
DESKTOP-M6HD4PO\lucch <giulia.lucchi7@studio.unibo.it>
2018-12-17 19:00:01 +0100

    Begin to change java structure

--- src/main/java/lbsStructure/LTSComputing.java
+++ src/main/java/lbsStructure/LTSComputing.java
@@ -10,32 +10,32 @@ import java.util.Iterator;
 import java.util.List;
 
 
+
 public class LTSComputing {
 
-    private final static String PROLOG_PATH = "src/main/prolog/LTSoperators.pl";
+    private final static String PROLOG_PATH = "src/main/prolog/LTSoperator.pl";
+    private final static int END = 0;
 
     private LabelTransitionSystemImpl labelTransitionSystem;
     private PrologUtils prologUtils;
     private Counter level;
-    private String inputCompute;
     private Converter converter;
+    boolean isFirst = true;
 
     public LTSComputing() throws IOException, InvalidTheoryException {
         level = new CounterImpl();
         prologUtils = new PrologUtilsImpl(PROLOG_PATH);
         converter = new ConverterImpl();
         labelTransitionSystem = LabelTransitionSystemImpl.getInstance();
-        inputCompute="[";
     }
 
     public void computeState() throws UnknownVarException, NoSolutionException, NoMoreSolutionException, InterruptedException {
         if(labelTransitionSystem.getLabelTransitionSystem().size()-1 <= level.getCounter()) {
             List<TransitionState> listAtLevel = labelTransitionSystem.getLabelTransitionSystem().get(level.getCounter());
             if(!(listAtLevel == null)) {
-
                 for (TransitionState transitionState : listAtLevel) {
                     int index = 0;
-                    String input = converter.inputConverter(transitionState.getFinalState().getValueState(), inputCompute);
+                    String input = "["+transitionState.getFinalState().getValueState()+","+ END +"]";
                     String goal ="rule("+ input + ", EV, FS).";
                     SolveInfo info = prologUtils.solveGoal(goal);
 
@@ -56,8 +56,8 @@ public class LTSComputing {
                         }
                     }
                 }
-                level.increment();
-                computeState();
+                //level.increment();
+               // computeState();
             }
         }
     }
@@ -65,24 +65,49 @@ public class LTSComputing {
     private void computeNewState(final SolveInfo info, final TransitionState transitionState, final String event,
                                  final String finalState, final int index) throws UnknownVarException, NoSolutionException, NoMoreSolutionException {
         if (!event.equals("0")) {
-            converter.getInputList().set(index, finalState);
-            String valueState = converter.outputConverter(converter.getInputList().toString());
+            //converter.getInputList().set(index, finalState);
+            String output = converter.outputConverter(finalState);
 
-                State valuateState = checkEqualState(valueState);
-                if(valuateState!= null){
-                    labelTransitionSystem.addTransitionState(level.getCounter() + 1, new TransitionStateImpl(
-                            transitionState.getFinalState(), valuateState, event));
-                }else {
-                    StateImpl newState = new StateImpl(valueState , level.getCounter() + 1);
-                    labelTransitionSystem.addTransitionState(level.getCounter() + 1, new TransitionStateImpl(
-                            transitionState.getFinalState(), newState, event));
-                    labelTransitionSystem.addState(newState);
-                    System.out.println("id: " +newState.getId() + "    value: "+newState.getValueState());
+            State equalState = checkEqualState(output);
+
+            TransitionState transitionState1 = null;
+            if (event.contains("'>'")) {
+                System.err.print("contiene >\n");
+                if (isFirst) {
+                    transitionState1= transitionState;
+                    System.err.print("transition state  "+transitionState.getFinalState().getId()+"\n ");
+                    System.err.print("transition state 1 "+transitionState1.getFinalState().getId()+"\n ");
+                    isFirst = false;
+                } else {
+                    System.err.print("contiene > else\n ");
+                    System.err.print("transition state  "+transitionState.getFinalState().getId()+"\n ");
+
+                    int i = labelTransitionSystem.getLabelTransitionSystem().size();
+                    System.err.print("size "+i+"\n ");
+                    transitionState1 = labelTransitionSystem.getLabelTransitionSystem().get(level.getCounter()+1)
+                            .get(i-1);
+                    System.err.print("transition state 1 "+transitionState1.getFinalState().getId()+"\n ");
+
                 }
+            }else {
+                System.err.print(" NON contiene > else\n ");
+                isFirst = true;
+                transitionState1 = transitionState;
             }
-
-            converter.reInitialization(index);
+            if (equalState != null) {
+                labelTransitionSystem.addTransitionState(level.getCounter() + 1, new TransitionStateImpl(
+                        transitionState1.getFinalState(), equalState, event));
+            } else {
+                StateImpl newState = new StateImpl(output, level.getCounter() + 1);
+                labelTransitionSystem.addTransitionState(level.getCounter() + 1, new TransitionStateImpl(
+                        transitionState1.getFinalState(), newState, event));
+                labelTransitionSystem.addState(newState);
+                System.out.println("id: " + newState.getId() + "    value: " + newState.getValueState() );
+            }
+        }
     }
+
+           // converter.reInitialization(index);
 
     private State checkEqualState(String valueState){
         for(Iterator<State> it = labelTransitionSystem.getAllStates().iterator(); it.hasNext();){

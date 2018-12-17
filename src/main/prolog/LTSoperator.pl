0982dc577cdd0dabfd5564df041a42ee225357e8
DESKTOP-M6HD4PO\lucch <giulia.lucchi7@studio.unibo.it>
2018-12-17 13:33:15 +0100

    Corrected rules

--- src/main/prolog/LTSoperator.pl
+++ src/main/prolog/LTSoperator.pl
@@ -6,9 +6,9 @@
 
 :- op(500, yfx, "plus").
 
-
 plus(D, plus(DD)).
 plus(D, plus(DD,T)).
+
 
 %rule([[plus(a, plus(b, plus(v, plus(s))))],[c,d]], E, F, P), (P=true->plus2list(E, LL), member(Y, LL)).
 
@@ -26,7 +26,7 @@ par2list(par(X),[X]).
 	%par2list(T, Z),!.
 par2list(par(X,Y), [X|Z]):-
 	par2list(Y,Z).
-	
+
 
 /* This function converts the process's sequence into parallel operator's sequence.
    list2par(+process parallel's list, -sequence of parallel operator).*/
@@ -37,7 +37,7 @@ list2par([H,HH], par(H, par(HH))).
 list2par([H|T1], par(H, par(HH,T))):-
  list2par(T1, par(HH,T)).
 
-%list2dot([], dot(0)).
+list2dot([], dot(0)).
 list2dot([H], dot(H)).
 list2dot([H,HH], dot(H, dot(HH))).
 list2dot([H|T1], dot(H, dot(HH,T))):-
@@ -67,34 +67,39 @@ dropElement(X,[H|Xs],L):-dropElement(X,Xs,L).
 member(E, [E | Xs], [], Xs).
 member(E, [X | Xs], [X | L], R) :-
     member(E, Xs, L, R).
-	
-/**/ 
-rule([plus(X, XS) | PP], EV, FS) :- 
-write('entraaa'), 	
-plus2list(plus(X, XS), XSS), 	
-member(C, XSS), 	
-(atom(C) 	-> 	FS=C
-;rule([C | PP], EV, FS)). 	 	 
 
 
-rule([par(X, XS) | PP], EV, FS) :- 	
-	par2list(par(X, XS), XSS), 	
-	member(C, XSS, L, R), 	
-	EV=C, 	
-	(atom(C) 	
-		->append(L, R, FS)
-		; rule([C|PP], EV, CFS), 
+/**/
+rule([plus(X, XS) | PP], EV, FS) :-
+	plus2list(plus(X, XS), XSS),
+	member(C, XSS),
+	(atom(C)
+	-> EV=C,	FS=C
+	;rule([C | PP], EV, FS)).
+
+rule([dot(X, XS) | PP], EV, FS):-
+ dot2list(dot(X, XS), XSS),
+ member(C, XSS, L, R),
+ EV=C,
+ (atom(C)
+ -> FS = R ;rule([C|PP], EV, FS)).
+
+rule([par(X, XS) | PP], EV, FS) :-
+	par2list(par(X, XS), XSS),
+	member(C, XSS, L, R),
+	(atom(C)
+	->EV=C, append([L, []], R, FS)
+	;	rule([C|PP], EV, CFS),
 	append(L, [CFS | R], FS)).
 
-%rule_parallel([[plus(a, plus(b))],[c,d]], E, F).
-rule([dot(X, XS) | PP], EV, FS):-
-	dot2list(dot(X, XS), XSS),
-	member(C, XSS, L, R),
-	EV = C,
-	(atom(C)
-		-> FS = R
-	; rule([C|PP], EV, FS)).
-	
 
-   
-%par2list(par([a], par([e])), L), member([X | P1], L).
+
+
+
+
+
+
+
+
+
+
